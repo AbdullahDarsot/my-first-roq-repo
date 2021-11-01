127b233dbc435b4d5015b454097ef12c3e61d8c3
Abdullah Darsot <abdullahdarsot1@gmail.com>
2021-11-01 13:52:06 +0000

    second commit

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 */
 //practise git stuff
 
-const sentence = 'and over'
+const sentence = 'and over '
 
-const output = `Repeat it over ${sentence.repeat (4)} again.`;
+const output = `Repeat it over ${sentence.repeat (10)} again.`;
 
-console.log(output)
+console.log(output);
+
